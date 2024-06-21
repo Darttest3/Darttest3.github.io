1987577b591e5fb2f5feb4fb9fc83b659914a0ff
xueshuangyang <163387498+xueshuangyang@users.noreply.github.com>
2024-06-21 09:39:16 +0000

    ADD

--- hello_world/bin/hello_world.dart
+++ hello_world/bin/hello_world.dart
@@ -18,5 +18,26 @@ void main(List<String> arguments) {
     'url': '//path/to/saturn.jpg'
   };
 
+  // 原有的代码
   print('Hello world: ${hello_world.calculate()}!');
+  
+  // 打印贵气体信息
+  var atomicNumberHelium = 2;
+  if (isNoble(atomicNumberHelium)) {
+    print('Element with atomic number $atomicNumberHelium is a noble gas: ${_nobleGases[atomicNumberHelium]}.');
+  } else {
+    print('Element with atomic number $atomicNumberHelium is not a noble gas.');
+  }
+}
+// 声明包含贵气体原子序号的映射
+Map<int, String> _nobleGases = {
+  2: 'Helium',
+  10: 'Neon',
+  18: 'Argon',
+  36: 'Krypton',
+  54: 'Xenon',
+  86: 'Radon',
+};
+isNoble(atomicNumber) {
+  return _nobleGases[atomicNumber] != null;
 }
