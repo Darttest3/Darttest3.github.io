240924d3e13a46060572dd10b0a0b69fe23c59d5
xueshuangyang <163387498+xueshuangyang@users.noreply.github.com>
2024-06-21 17:30:46 +0800

    hello_world.dart

--- hello_world/bin/hello_world.dart
+++ hello_world/bin/hello_world.dart
@@ -1,5 +1,22 @@
 import 'package:hello_world/hello_world.dart' as hello_world;
+// Importing core libraries
+import 'dart:math';
+
+// Importing libraries from external packages
+import 'package:test/test.dart';
+
+// Importing files
+import 'path/to/my_other_file.dart';
 
 void main(List<String> arguments) {
+  var name = 'Voyager I';
+  var year = 1977;
+  var antennaDiameter = 3.7;
+  var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+  var image = {
+    'tags': ['saturn'],
+    'url': '//path/to/saturn.jpg'
+  };
+
   print('Hello world: ${hello_world.calculate()}!');
 }
